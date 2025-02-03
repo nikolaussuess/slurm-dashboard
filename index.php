71c73fd23bab91f82f9e23ca1d38c4136b8b09c4
Nikolaus Süß <nikolaus.suess@univie.ac.at>
2025-02-03 21:46:15 +0100

    Fix

--- index.php
+++ index.php
@@ -137,9 +137,9 @@ if( isset($_SESSION['USER']) ){
                     $gpus_used = preg_replace('/.*:(\d+)(?:\(.*\))?$/', '$1', $gres_used);
                     #$gpus = preg_replace('/.*gpu:(\d+).*|.*gpu:\(null\):(\d+).*/', '$1$2', $gres);
                     #$gpus_used = preg_replace('/.*gpu:(\d+).*|.*gpu:\(null\):(\d+).*/', '$1$2', $gres_used);
-                    echo "GPUs='$gpus', gpus_used='$gpus_used', gres='$gres', gres_used='$gres_used'";
-                    #$gpus_percentage = (int)$gpus_used / (int)$gpus * 100;
-                    $gpus_percentage = 1;
+                    // For debugging
+                    #echo "GPUs='$gpus', gpus_used='$gpus_used', gres='$gres', gres_used='$gres_used'";
+                    $gpus_percentage = (int)$gpus_used / (int)$gpus * 100;
                 }
                 $templateBuilder->setParam("GPU_PERCENTAGE", $gpus_percentage);
                 $templateBuilder->setParam("GPU_USED", $gpus_used);

e095cc9fec1bfb1daaaad5673d0777c36ac8ed2e
Nikolaus Süß <nikolaus.suess@univie.ac.at>
2025-11-07 18:45:49 +0100

    Test

--- utils.inc.php
+++ utils.inc.php
@@ -12,29 +12,6 @@ function get_number_if_defined(array $arr, string $default = 'undefined') : stri
         return $default;
 }
 
-function read_exit_code(array $job_arr): string {
-    if(! isset($job_arr['exit_code'])){
-        return "?";
-    }
-
-    if(isset($job_arr['exit_code']['return_code'])){
-        if(isset($job_arr['exit_code']['signal']) && isset($job_arr['exit_code']['signal']['name']) && $job_arr['exit_code']['signal']['id']['set'])
-            return get_number_if_defined($job_arr['exit_code']['return_code']) . " with Signal " . $job_arr['exit_code']['signal']['name'] . ' (' . $job_arr['exit_code']['signal']['id']['number'] . ')';
-        else
-            return get_number_if_defined($job_arr['exit_code']['return_code']);
-    }
-    else {
-        return get_number_if_defined($job_arr['exit_code']);
-    }
-}
-
-function get_date_from_unix(array $job_arr, string $param): string {
-    if(! isset($job_arr[$param]) || $job_arr[$param] == 0){
-        return "?";
-    }
-
-    return date('Y-m-d H:i:s', $job_arr[$param]);
-}
 
 function get_date_from_unix_if_defined(array $job_arr, string $param, string $default = 'undefined') : string {
     if(! isset($job_arr[$param])){
@@ -54,49 +31,6 @@ function get_date_from_unix_if_defined(array $job_arr, string $param, string $de
         else {
             return date('Y-m-d H:i:s', $job_arr[$param]['number']);
         }
-    }
-    else {
-        return $default;
-    }
-}
-
-/**
- * Elapsed time is in seconds. Display it accordingly.
- * @param $job_arr array Job array as from JSON
- * @param $param string Array index (e.g. elapsed)
- * @return string The time in D-HH:MM:SS
- */
-function get_elapsed_time(array $job_arr, string $param = 'elapsed'): string {
-    if(! isset($job_arr[$param]) || $job_arr[$param] == 0 ){
-        return "?";
-    }
-
-    $totalSeconds = $job_arr[$param]; // Assuming $job_arr[$param] is in seconds
-    $days = floor($totalSeconds / 86400); // 86400 seconds in a day
-    $hours = floor(($totalSeconds % 86400) / 3600); // 3600 seconds in an hour
-    $remainingMinutes = floor(($totalSeconds % 3600) / 60); // Remaining minutes
-    $remainingSeconds = $totalSeconds % 60; // Remaining seconds
-
-    return sprintf('%d-%02d:%02d:%02d', $days, $hours, $remainingMinutes, $remainingSeconds);
-}
-
-/**
- * Time limit is in minutes, so the input is minutes.
- * @param $job_arr array Job array as from JSON
- * @param $param string Array index (e.g. time_limit)
- * @param $default string what to return if not set.
- * @return string The time limit in D-HH:MM:SS
- */
-function get_timelimit_if_defined(array $job_arr, string $param, string $default = 'undefined'): string {
-    if(! isset($job_arr[$param])){
-        return "?";
-    }
-
-    if($job_arr[$param]['set']){
-        $days = floor($job_arr[$param]['number'] / 1440); // 1440 minutes in a day
-        $hours = floor(($job_arr[$param]['number'] % 1440) / 60); // 60 minutes in an hour
-        $remainingMinutes = $job_arr[$param]['number'] % 60; // Remaining minutes
-        return sprintf('%d-%02d:%02d:00', $days, $hours, $remainingMinutes);
     }
     else {
         return $default;
@@ -133,11 +67,4 @@ function get_job_state_view(array $job, string $param_name = 'job_state'): strin
         $job_state_text .= '<span class="badge" style="background-color: ' . $state_color . '">' . $job_state . '</span> ';
     }
     return $job_state_text;
-}
-
-function get_nodes(array $job_arr) : string {
-    if(isset($job_arr['job_resources']) && isset($job_arr['job_resources']['nodes']))
-        return $job_arr['job_resources']['nodes'];
-    else
-        return "?";
 }

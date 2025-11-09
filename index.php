f13df6cf375b54dce468480b64861f6a8318cdec
Nikolaus Süß <nikolaus.suess@univie.ac.at>
2025-11-09 15:58:09 +0100

    Slurm queue

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require_once 'view/login.tpl.php';
 require_once 'view/maintenances.tpl.php';
 require_once 'view/usage.tpl.php';
 require_once 'view/job.tpl.php';
+require_once 'view/slurm-queue.tpl.php';
 
 $dao = \client\ClientFactory::newClient();
 $title = "Clusterinfo " . CLUSTER_NAME;
@@ -109,87 +110,17 @@ if( isset($_SESSION['USER']) ){
             break;
 
         case "jobs":
-            $contents .= "<h2>Jobs</h2>";
-
-            // Allow to exclude jobs of partition p_low
-            // Every user can use partition p_low. The aim of the partition p_low is to use
-            // any (currently) unused resources for experiments that should finish at some point,
-            // but it does not matter when. Any normal job will have higher priority than a job in
-            // p_low and will hence interrupt (REQUEUE) these jobs.
-            //
-            // The filtering is done locally on the web server.
-            // JavaScript is required.
-            $contents .= <<<EOF
-<div class="form-check form-switch">
-  <input 
-        class="form-check-input" 
-        type="checkbox" 
-        role="switch" 
-        id="show_p_low" 
-        onclick="if( this.checked ) window.location.href = '?action=jobs&exclude_p_low=1'; else window.location.href = '?action=jobs';"
-EOF;
-            if(isset($_GET['exclude_p_low']) && $_GET['exclude_p_low'] == 1)
-                $contents .= ' checked ';
-$contents .= <<<EOF
-        >
-  <label 
-        class="form-check-label" 
-        for="show_p_low">
-            Hide partition <span title="Every user can use partition p_low. The aim of the partition p_low is to use any (currently) unused resources for experiments that should finish at some point, but it does not matter when. Any normal job will have higher priority than a job in p_low and will hence interrupt (REQUEUE) these jobs."><span class="monospaced">p_low</span></span>
-  </label>
-</div>
-EOF;
 
             // Filter
             // Exclude partition p_low if parameter exclude_p_low=1
+            $exclude_p_low = isset($_GET['exclude_p_low']) && $_GET['exclude_p_low'] == 1;
             $filter = array();
-            if(isset($_GET['exclude_p_low']) && $_GET['exclude_p_low'] == 1)
+            if($exclude_p_low)
                 $filter['exclude_p_low'] = 1;
 
             $jobs = $dao->get_jobs($filter);
 
-            $contents .= '<div>Found <span style="font-weight: bold">' . count($jobs) . ' jobs</span>.</div>';
-
-            $contents .= <<<EOF
-<div class="table-responsive">
-    <table class="tableFixHead table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Partition</th>
-                <th>User</th>
-                <th>State</th>
-                <th>Start time</th>
-                <th>Time limit</th>
-                <th># Nodes</th>
-                <th>Nodelist</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-EOF;
-
-            foreach( $jobs as $job ) {
-
-                $contents .= "<tr>";
-                $contents .=    "<td>" . $job['job_id'] . "</td>";
-                $contents .=    "<td>" . $job['job_name'] . "</td>";
-                $contents .=    "<td>" . $job['partition'] . "</td>";
-                $contents .=    "<td>" . $job['user_name'] . " (" . $job['user_id'] . ")</td>";
-                $contents .=    "<td>" . \utils\get_job_state_view($job) . "</td>";
-                $contents .=    "<td>" . $job['time_start'] . "</td>";
-                $contents .=    "<td>" . $job['time_limit'] . "</td>";
-                $contents .=    "<td>" . ($job['node_count'] != NULL ? $job['node_count'] : "?") . "</td>";
-                $contents .=    "<td>" . $job['nodes'] . "</td>";
-                $contents .=    '<td><a href="?action=job&job_id=' . $job['job_id'] . '">[Details]</a></td>';
-
-            }
-            $contents .= <<<EOF
-        </tbody>
-    </table>
-</div>
-EOF;
+            $contents .= \view\actions\get_slurm_queue($jobs, $exclude_p_low);
 
             break;
 

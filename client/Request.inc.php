d42bfa6a1ca0a5f03fdf0a92f6d7b39d6792c2c6
Nikolaus Süß <nikolaus.suess@univie.ac.at>
2026-01-18 19:29:26 +0100

    Debug

--- client/Request.inc.php
+++ client/Request.inc.php
@@ -68,7 +68,8 @@ class UnixRequest implements Request {
         #print_r($body);
         #print "</pre>";
         if(isset($_SESSION['USER']) && $_SESSION['USER'] == 'suessn98' && str_starts_with($endpoint, 'shares')){
-            file_put_contents(__DIR__ .'/../test.json', $body);
+            #file_put_contents(__DIR__ .'/../test.json', $body);
+            $body = str_replace('"level": Infinity', '"level": "Infinity"', $body);
         }
 
         // Decode the JSON response
